c56b0165c068d32495d9082cb646c3dd8b3d1024
tenminuteversity <kibria7533>
2022-06-13 23:27:23 +0600

    queue command added

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use Illuminate\Support\Facades\Route;
+
+Route::get('/start-queue', function () {
+    \Illuminate\Support\Facades\Artisan::call('queue:work',['--queue'=>'stock.q']);
+});
 $router->get('/', function () {
     return 'Hello World from stock service';
 });
